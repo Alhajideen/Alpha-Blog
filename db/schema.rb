87064520840f1d451c7d9509a04cae1d1ae184d4
alhajideen <salifunurudeen834@gmail.com>
2023-01-18 22:39:24 +0000

    create foreign key on posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_203445) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_223801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_203445) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_203445) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
