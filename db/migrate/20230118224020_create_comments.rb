7feb6706e3a1bef43af5e0ed8f083a9bd259932d
alhajideen <salifunurudeen834@gmail.com>
2023-01-18 22:51:08 +0000

    add linter files

--- db/migrate/20230118224020_create_comments.rb
+++ db/migrate/20230118224020_create_comments.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.text :text 
+      t.text :text
       t.references :author, foreign_key: { to_table: :users }
       t.references :post, foreign_key: { to_table: :posts }
-      
+
       t.timestamps
     end
   end
